ba4989208f6737abc450468003e7fc50c0716727
Dave Harned <dharned@gmail.com>
2019-03-28 19:36:25 -0500

    initialize project and basic public layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'podcast-directory';
+  podcasts = [
+    {
+      title: 'Junior Developer Toolbox',
+      podcastStatus: 'Active Podcast',
+      howLongPodcasting: '1 - 2 Years',
+      hosts: [
+        { firstName: 'Erin', lastName: 'Orstrom' },
+        { firstName: 'Dave', lastName: 'Harned' }
+      ],
+      description:
+        'Bringing you real talk from the trenches about everyday life as a Junior Developer.',
+      imageUrl:
+        'https://juniordevelopertoolbox.com/wp-content/uploads/2017/09/v3-100x103.png',
+      website: 'https://juniordevelopertoolbox.com',
+      rssFeed: 'https://juniordevelopertoolbox.com/feed/',
+      iTunesLink:
+        'https://itunes.apple.com/us/podcast/junior-developer-toolbox/id1290011103?mt=2',
+      emailAddress: 'helloworld@juniordevelopertoolbox.com',
+      twitterHandle: '@jrdevtoolbox',
+      instagramName: 'juniordevelopertoolbox'
+    },
+    {
+      title: 'Navigate',
+      podcastStatus: 'Active Podcast',
+      howLongPodcasting: '2 - 3 Years',
+      hosts: [
+        { firstName: 'Clark', lastName: 'Buckner' },
+        { firstName: 'Brynn', lastName: 'Plummer' }
+      ],
+      description:
+        'Think "NPR’s How I Built This" meets "Mister Rogers’ Neighborhood."',
+      imageUrl: '',
+      website: 'https://www.ec.co/podcast',
+      rssFeed: '',
+      iTunesLink: '',
+      emailAddress: '',
+      twitterHandle: '',
+      instagramName: ''
+    }
+  ];
 }
