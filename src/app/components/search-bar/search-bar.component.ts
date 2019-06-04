57bda878d4fb5a22cc2ae8a76f97871f33dcfbdf
Dave Harned <dharned@gmail.com>
2019-06-03 21:19:07 -0500

    add apple categories to search bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PodcastDataService } from './../../services/podcast-data.service';
+
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.sass']
 })
 export class SearchBarComponent implements OnInit {
+  categories: string[];
 
-  constructor() { }
+  constructor(private podcastdataService: PodcastDataService) {}
 
   ngOnInit() {
+    this.categories = this.podcastdataService.getCategories();
   }
-
 }
