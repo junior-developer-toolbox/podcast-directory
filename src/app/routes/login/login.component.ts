00e1f1559f6dd59e040c3c3aa7358567f69c58f7
Dave Harned <dharned@gmail.com>
2019-06-15 15:42:01 -0500

    build and test initial login / reg functions

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,28 +12,47 @@ export class LoginComponent implements OnInit {
   emailInput = new FormControl('');
   passwordInput = new FormControl('');
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
   loginUser() {
-    console.log('Logging in:');
-    console.table({
-      email: this.emailInput.value,
-      password: this.passwordInput.value
-    });
+    console.log('Logging in:', this.emailInput.value);
+    this.authService
+      .login(this.emailInput.value, this.passwordInput.value)
+      .then(
+        res => {
+          console.log(res);
+          console.log('success', 'Successfully Logged In!');
+        },
+        err => {
+          console.log('danger', err.message);
+        }
+      );
   }
 
   registerUser() {
-    console.log('Registering:');
-    console.table({
-      email: this.emailInput.value,
-      password: this.passwordInput.value
-    });
+    console.log('Registering:', this.emailInput.value);
+    this.authService
+      .register(this.emailInput.value, this.passwordInput.value)
+      .then(
+        res => {
+          console.log(res);
+          console.log('success', 'Successfully Registered!');
+        },
+        err => {
+          console.log('danger', err.message);
+        }
+      );
   }
 
   resetUser() {
     console.log('Resetting:');
     console.table({ email: this.emailInput.value });
+  }
+
+  logout() {
+    this.authService.logout();
+    console.log('Logging out');
   }
 }
